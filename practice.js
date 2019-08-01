e7999fb3a215f8167e0202453d36b7d8358d0371
Dan Storm <stormdanp@gmail.com>
2019-07-31 21:54:05 -0500

    worked through 3 separate exercises they are commented out

---
+++ practice.js
@@ -0,0 +1,32 @@
+console.log("you're doing great!")
+
+
+//     for(let i = 1; i <= 100; i++){
+    
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log(`${i}: chicken monkey`)
+//     }
+//     else if(i % 3 === 0){
+//         console.log(`${i}: chicken`)
+//     }
+//     else if(i % 5 === 0){
+//         console.log(`${i}: monkey`)
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+
+// const findVowels = str => {
+//     let count = 0
+//     const vowels = ['a', 'e', 'i', 'o', 'u']
+//     for(let char of str.toLowerCase()){
+//         if(vowels.includes(char)){
+//             count ++
+//         }
+//     }
+//     return count
+// }
+
+// console.log(findVowels("oooooo"));
+
